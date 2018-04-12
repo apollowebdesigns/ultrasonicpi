e3588bbff4b2c29a14791c6062e6fab6776d5699
Andy Evans <andrewevans0@outlook.com>
2018-04-12 19:28:02 +0100

    added second distance function

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import time
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
 
+GPIO_ZERO_TRIGGER = 12
+GPIO_ZERO_ECHO = 18
+
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
 
@@ -47,37 +50,37 @@ def distance():
 
     return distance
 
-# def distanceWithSecondPi():
-#     # set Trigger to HIGH
-#     pi_zero.write(GPIO_TRIGGER, 1)
-#
-#     # set Trigger after 0.01ms to LOW
-#     time.sleep(0.00001)
-#     pi_zero.write(GPIO_TRIGGER, 0)
-#
-#     StartTime = time.time()
-#     StopTime = time.time()
-#
-#     # save StartTime
-#     while pi_zero.read(GPIO_ECHO) == 0:
-#         StartTime = time.time()
-#
-#     # save time of arrival
-#     while pi_zero.read(GPIO_ECHO) == 1:
-#         StopTime = time.time()
-#
-#     # time difference between start and arrival
-#     TimeElapsed = StopTime - StartTime
-#     # multiply with the sonic speed (34300 cm/s)
-#     # and divide by 2, because there and back
-#     distance = (TimeElapsed * 34300) / 2
-#
-#     return distance
+def distanceWithSecondPi():
+    # set Trigger to HIGH
+    pi_zero.write(GPIO_TRIGGER, 1)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    pi_zero.write(GPIO_TRIGGER, 0)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while pi_zero.read(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while pi_zero.read(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
+            dist = distanceWithSecondPi()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
 
