1ef10797b6b914c8e6a165d9e66b6973afcc8528
Andy Evans <andrewevans0@outlook.com>
2018-04-12 19:09:41 +0100

    added second distance function

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def distanceWithSecondPi():
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
+            dist = distanceWithSecondPi()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
 
