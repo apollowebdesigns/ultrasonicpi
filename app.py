d22b177da784547a60b62a7d6a61bd28bf6c0518
Andy Evans <andrewevans0@outlook.com>
2018-04-14 12:28:51 +0100

    added new distance calculator

--- app.py
+++ app.py
@@ -1,54 +1,10 @@
-import RPi.GPIO as GPIO
-import pigpio
-import time
-
 import gevent
 import gevent.monkey
 from gevent.pywsgi import WSGIServer
 gevent.monkey.patch_all()
 
 from flask import Flask, request, Response, render_template
-
-#set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
-
-GPIO_ZERO_TRIGGER = 12
-GPIO_ZERO_ECHO = 18
-
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
-
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-
-    return distance
+from distancecalculator import distance
 
 app = Flask(__name__)
 
@@ -78,15 +34,3 @@ if __name__ == '__main__':
         print("Measurement stopped by User")
 
         GPIO.cleanup()
-
-# if __name__ == '__main__':
-#     try:
-#         while True:
-#             dist = distance()
-#             print ("Measured Distance = %.1f cm" % dist)
-#             time.sleep(1)
-#
-#             # Reset by pressing CTRL + C
-#     except KeyboardInterrupt:
-#         print("Measurement stopped by User")
-#         GPIO.cleanup()
